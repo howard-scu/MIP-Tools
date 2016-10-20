4825a1416bd945090cd93833182818bd033c63a9
luxiaohua <luxiaohua@outlook.com>
2016-10-20 14:17:18 +0800

    add converter for dicom to mha

---
+++ ConvertDcmToMha.cpp
@@ -0,0 +1,53 @@
+#include <vtkImageData.h>
+#include <vtkSmartPointer.h>
+#include <vtkDICOMImageReader.h>
+#include <itkVTKImageToImageFilter.h>
+#include "itkImageFileWriter.h"
+#include "cmdline.h"
+using namespace std;
+
+typedef itk::Image< unsigned short, 3 >		ImageType;
+
+void ConvertDcmToMha(const char* path, const char* filename)
+{
+	// use vtk to read dicom
+	vtkSmartPointer<vtkDICOMImageReader> reader =
+		vtkSmartPointer<vtkDICOMImageReader>::New();
+	reader->SetDirectoryName(path);
+	reader->Update();
+
+	// convert vtkimagedata to itkimage
+	typedef itk::VTKImageToImageFilter<ImageType>		VTK2ITK;
+
+	VTK2ITK::Pointer v2ifilter = VTK2ITK::New();
+	v2ifilter->SetInput(reader->GetOutput());
+	v2ifilter->Update();
+
+	// output meta image
+	typedef  itk::ImageFileWriter<ImageType>			WriterType;
+	WriterType::Pointer writer = WriterType::New();
+	writer->SetFileName(filename);
+	writer->SetInput(v2ifilter->GetOutput());
+	try
+	{
+		writer->Update();
+	}
+	catch (itk::ExceptionObject & excep)
+	{
+		std::cerr << "Exception caught !" << std::endl;
+		std::cerr << excep << std::endl;
+	}
+}
+
+
+int main(int argc,char* argv[])
+{
+	cmdline::parser a;
+	a.add<string>("path", 'p', "dicom path");
+	a.add<string>("filename", 'f', "meta file name");
+	a.parse_check(argc, argv);
+
+	ConvertDcmToMha(a.get<string>("path").c_str(),	/* dicom dataset directory */
+		a.get<string>("filename").c_str());			/* mha file name(.mha)*/
+}
+
